825421d142cf9fe8df68d3b80dd8c7dcc892340a
Hunter Wright <wright.hunter@gmail.com>
2018-02-02 13:19:54 -0600

    tacobell class added, started fixing the TacoParser.

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using log4net;
 
 
@@ -21,8 +22,43 @@ namespace LoggingKata
         public ITrackable Parse(string line)
         {
             //DO not fail if one record parsing fails, return null
-            return null; //TODO Implement
+            //TODO Implement
+            
+            var cells = line.Split(',');
+            
+            if (cells.Length < 3)
+            {
+                Logger.Error("Something went wrong");
+                return null;
+            }
+
+            var lon = double.Parse(cells[0]);
+            var lat = double.Parse(cells[1]);
+
+
+
         }
-       
+
+
+
+
+
+        // grab the long from your array at index 0
+        // grab the lat from your array at index 1
+        // grab the name from your array at index 2
+
+        // Your going to need to parse your string as a `double`
+        // which is similar to parse a string as an `int`
+
+        // You'll need to create a TacoBell class
+        // that conforms to ITrackable
+
+        // Then, you'll need an instance of the TacoBell class
+        // With the name and point set correctly
+
+        // Then, return the instance of your TacoBell class
+        // Since it conforms to ITrackable
+
+
     }
 }
